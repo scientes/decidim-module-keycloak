4d7919fd6f17ee8e56b15292fe2b309bb1d354da
Bastian Große <5737628-scientes@noreply.gitlab.com>
2025-12-28 16:41:49 +0100

    bump version to 31

--- lib/decidim/keycloak/version.rb
+++ lib/decidim/keycloak/version.rb
@@ -3,8 +3,8 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Keycloak
-    VERSION = "0.1.1"
-    DECIDIM_VERSION = "0.29.2"
-    COMPAT_DECIDIM_VERSION = [">= 0.29.0", "< 0.30"].freeze
+    VERSION = "0.2.0"
+    DECIDIM_VERSION = "0.31.0"
+    COMPAT_DECIDIM_VERSION = [">= 0.29.0", "< 0.32"].freeze
   end
 end
